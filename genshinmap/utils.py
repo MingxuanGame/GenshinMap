0c9abacc7ad8f5bd6c89d714a1ad21042211657b
MingxuanGame <MingxuanGame@outlook.com>
2022-10-01 01:28:37 +0800

    :sparkles: add cropping maps by points

--- genshinmap/utils.py
+++ genshinmap/utils.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+from math import ceil
 from io import BytesIO
-from typing import List, Union
+from typing import List, Tuple, Union
 from asyncio import gather, create_task
 
 from PIL import Image
@@ -72,9 +73,7 @@ async def get_map_by_pos(
     返回：
         `PIL.Image.Image` 对象
     """
-    # 4 * (y // 4096) {0,4,8}
-    # x // 4096 {0,1,2,3}
-    return await get_img(map.slices[4 * (int(y // 4096)) + int(x // 4096)])
+    return await get_img(map.slices[_pos_to_index(x, y)])
 
 
 def get_points_by_id(id_: int, points: List[Point]) -> List[XYPoint]:
@@ -120,3 +119,102 @@ def convert_pos(points: List[XYPoint], origin: List[int]) -> List[XYPoint]:
         [XYPoint(x=6044, y=9335), XYPoint(x=644, y=6135)]
     """
     return [XYPoint(x + origin[0], y + origin[1]) for x, y in points]
+
+
+def convert_pos_crop(
+    top_left_index: int, points: List[XYPoint]
+) -> List[XYPoint]:
+    """
+    根据左上角地图切片的索引转换坐标（已经通过 `convert_pos` 转换）
+
+    参数：
+        top_left_index: `int`
+            左上角地切片图的索引
+
+        points: `list[XYPoint]`
+            米游社资源坐标（已经通过 `convert_pos` 转换）
+
+    返回：
+        `list[XYPoint]`
+
+    示例：
+        >>> from genshinmap.models import XYPoint
+        >>> points = [XYPoint(0, 0), XYPoint(20, 20)]
+        >>> convert_pos_crop(0, points)
+        [XYPoint(x=0, y=0), XYPoint(x=20, y=20)]
+        >>> convert_pos_crop(1, points)
+        [XYPoint(x=-4096, y=0), XYPoint(x=-4076, y=20)]
+        >>> convert_pos_crop(4, points)
+        [XYPoint(x=0, y=-4096), XYPoint(x=20, y=-4076)]
+        >>> convert_pos_crop(5, points)
+        [XYPoint(x=-4096, y=-4096),XYPoint(x=-4076, y=-4076)]
+    """
+    y, x = divmod(top_left_index, 4)
+    if x == y == 0:
+        return points
+    x *= 4096
+    y *= 4096
+    result = []
+    for point in points:
+        px, py = point
+        result.append(XYPoint(px - x, py - y))
+    return result
+
+
+def _pos_to_index(x: Union[int, float], y: Union[int, float]) -> int:
+    # 4 * (y // 4096) {0,4,8}
+    # x // 4096 {0,1,2,3}
+    return 4 * (int(y // 4096)) + int(x // 4096)
+
+
+def _generate_matrix(
+    top_left: int, top_right: int, bottom_left: int
+) -> List[int]:
+    result = []
+    while True:
+        result.extend(iter(range(top_left, top_right + 1)))
+        if top_left == bottom_left:
+            break
+        top_left_copy = top_left
+        top_left += 4
+        top_right = top_left + (top_right - top_left_copy)
+    return result
+
+
+def crop_image_and_points(
+    points: List[XYPoint],
+) -> Tuple[List[int], int, List[XYPoint]]:
+    """
+    根据坐标（需通过 `convert_pos` 转换）计算地图切片索引，间隔（即贴完一张图片后还需要贴几张才换行）和转换后的坐标
+
+    参数：
+        points: `list[XYPoint]`
+            米游社资源坐标（已经通过 `convert_pos` 转换）
+
+    返回：
+        `tuple[list[int], int, list[XYPoint]]`
+
+        第 1 个元素为地图切片索引的列表
+        第 2 个元素为间隔
+        第 3 个元素为使用 `convert_pos_crop` 转换后的坐标
+
+    示例：
+        >>> points = [XYPoint(x=4200, y=8000), XYPoint(x=4150, y=10240)]
+        >>> crop_image_and_points(points)
+        ([5, 9], 0, [XYPoint(x=104, y=3904), XYPoint(x=54, y=6144)])
+    """
+    xs = [p.x for p in points]
+    ys = [p.y for p in points]
+    x1, y1 = min(xs), min(ys)
+    x2, y2 = max(xs), max(ys)
+
+    x1 = int(x1 // 4096 * 4096)
+    x2 = x1 if x1 + 4096 >= x2 else ceil(x2 / 4096) * 4096 - 4096
+    y1 = int(y1 // 4096 * 4096)
+    y2 = y1 if y1 + 4096 >= y2 else ceil(y2 / 4096) * 4096 - 4096
+    index_x1, index_x2 = _pos_to_index(x1, y1), _pos_to_index(x2, y1)
+    return (
+        _generate_matrix(index_x1, index_x2, _pos_to_index(x1, y2)),
+        index_x2 - index_x1,
+        convert_pos_crop(index_x1, points),
+    )
